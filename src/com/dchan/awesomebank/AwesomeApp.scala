0dd0cd380cfda8483f2e5a29d5a4b5ab62464a2d
Dulitha Rasanga Wijewantha <dulithaz@gmail.com>
2012-12-30 22:46:48 +0530

    Working on the basic interface and learning how to use Scala Swing

---
+++ src/com/dchan/awesomebank/AwesomeApp.scala
@@ -0,0 +1,35 @@
+package com.dchan.awesomebank
+
+import scala.swing.SimpleSwingApplication
+import scala.swing.MainFrame
+import scala.swing.Button
+import javax.swing.JPanel
+import java.awt.BorderLayout
+import scala.swing.FlowPanel
+import scala.swing.TextField
+import scala.swing.Label
+import scala.swing.TextArea
+import scala.swing.Action
+import scala.swing.event.ButtonClicked
+
+object AwesomeApp extends SimpleSwingApplication {
+  
+  def createTxt = new TextField{
+    columns = 5
+  }
+  def createTxArea = new TextArea{columns=10;rows=10; }
+  val eventTxt =createTxt
+  val descriptionTxt =createTxArea
+  val confirmButton = new Button{text = "Remember"}
+  def top = new MainFrame {
+    title = "Hellow World"
+    contents = new FlowPanel(
+      new Label("Event"),eventTxt, new Label("Description"),descriptionTxt, confirmButton
+    )
+  }
+  listenTo(confirmButton)
+  reactions += {
+    case e: ButtonClicked=> println("Awesome Remembered")
+  }
+
+}
